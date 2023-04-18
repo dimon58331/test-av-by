aa53dd91d7f437f3437de8a2a22b0ad7d567d770
Dzmitry <ifrez5833@gmail.com>
2023-04-19 00:44:50 +0300

    Initialize a new project

---
+++ server/src/main/java/by/av/test/testavby/TestAvByApplication.java
@@ -0,0 +1,13 @@
+package by.av.test.testavby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAvByApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAvByApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/by/av/test/testavby/TestAvByApplicationTests.java
@@ -0,0 +1,13 @@
+package by.av.test.testavby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAvByApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
