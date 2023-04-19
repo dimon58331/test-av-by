062f8765f712726ad0f84ae96fa13b2d866f88c5
Dzmitry <ifrez5833@gmail.com>
2023-04-19 13:21:33 +0300

    Initialize UserService

---
+++ server/src/main/java/by/av/test/testavby/service/UserService.java
@@ -0,0 +1,17 @@
+package by.av.test.testavby.service;
+
+import by.av.test.testavby.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
